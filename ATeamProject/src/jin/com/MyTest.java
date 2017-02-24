91814f55684c673751255b19311baf7c45464e24
tabrisjin <tabrisjin@naver.com>
2017-02-24 12:40:40 +0900

    8

--- ATeamProject/src/jin/com/MyTest.java
+++ ATeamProject/src/jin/com/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Test By Jin");
-		System.out.println("Git Test 2");
+		System.out.println("Git Test 2");  
 	}
 
 }
